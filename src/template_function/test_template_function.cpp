11635d7d79415ba4ac06bd3afd6b05dab16f28bc
Mauri Mustonen <mauri_mustonen@hotmail.com>
2019-08-29 11:50:14 +0300

    google test working

--- src/template_function/test_template_function.cpp
+++ src/template_function/test_template_function.cpp
@@ -1,6 +1,3 @@
-#ifndef TEST_TEMPLATE_FUNCTION
-#define TEST_TEMPLATE_FUNCTION
-
 #include "template_function.cpp"
 #include <gtest/gtest.h>
 
@@ -8,5 +5,3 @@ TEST(TestTemplateFunction, testInt) {
 	ASSERT_EQ(get_max(10, 11), 11);
 	ASSERT_EQ(get_max<int>(11, 12), 12);
 }
-
-#endif
