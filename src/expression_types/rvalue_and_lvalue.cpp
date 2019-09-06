70ed4101b186b2d8b470644eb1019547fba5ef81
Mauri Mustonen <mauri_mustonen@hotmail.com>
2019-09-06 14:05:47 +0300

    finish up rvalue example

--- src/expression_types/rvalue_and_lvalue.cpp
+++ src/expression_types/rvalue_and_lvalue.cpp
@@ -7,10 +7,22 @@ int return_rvalue()
 }
 
 int global_int = 11;
-// Function returning lvalue of the global interger with reference to int (int&).
+// Function returning lvalue of the global integer with reference to int (int&).
 int& return_lvalue()
 {
     return global_int;
+}
+
+// This function definition takes non-constant lvalue reference as an argument.
+void take_non_const_reference(int& value)
+{
+    std::cout << "non-constant lvalue reference: " << value << std::endl;
+}
+
+// This function definition takes constant lvalue reference as an argument.
+void take_const_reference(const int& value)
+{
+    std::cout << "constant lvalue reference: " << value << std::endl;
 }
 
 /*
@@ -23,7 +35,7 @@ int main()
 {
     // 12 is an rvalue and x is an lvalue.
     int x = 12;
-    // Address-of operator & takes an lvalue and convers it to rvalue.
+    // Address-of operator & takes an lvalue and converts it to rvalue.
     int* y = &x;
     // Prints "error: lvalue required as left operand of assignment". This is because lvalue is required
     // for the assignment.
@@ -31,7 +43,7 @@ int main()
     // Prints "error: lvalue required as unary '&' operand".
     // int* y = &13;
 
-    // Illegal assingment with message "error: lvalue required as left operand of assignment".
+    // Illegal assignment with message "error: lvalue required as left operand of assignment".
     // return_rvalue() = 10;
     // Valid assignment because function returning lvalue.
     return_lvalue() = 10;
@@ -41,6 +53,20 @@ int main()
     int& a_ref = a;
     a_ref++; // a == 11
     std::cout << "a: " << a << std::endl;
+
+    // When function is taking a non-constant lvalue reference as a parameter then rvalue
+    // (value 10) cannot be given because it's illegal rvalue to lvalue
+    // conversion. Commented line below fails with error: cannot bind non-const lvalue reference of
+    // type ‘int&’ to an rvalue of type ‘int’.
+    // take_non_const_reference(10);
+
+    // Instead storing 10 to lvalue first and then giving it as parameter works
+    // fine.
+    int value = 10;
+    take_non_const_reference(value);
+    // However if function takes constant lvalue reference then it's possible
+    // to give rvalue as parameter without first saving it to lvalue.
+    take_const_reference(10);
 }
 
 
